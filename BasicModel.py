fix bug in build accuracy: label and prediction in calling tf.confusion_matrix are in wrong order

BasicModel.py
@@ -113,6 +113,7 @@ class Model(object):
         self.moving_average_train_op = None
         self.moving_average_maintainer = None
         self.q_8_accuracy = None
+        self.confusion_mat = None
         self.fetches = None
         self.filename_queue = None
         self.rnn_output = None
@@ -164,6 +165,7 @@ class Model(object):
                 "loss": self.loss,
                 "objective": self.moving_average_train_op,
                 "evaluation": self.q_8_accuracy,
+                "confusion_matrix": self.confusion_mat,
                 "summary": summary_op
             }
         elif self.mode == "valid" or self.mode == "test":
@@ -180,6 +182,7 @@ class Model(object):
             self.fetches = {
                 "loss": self.loss,
                 "evaluation": self.q_8_accuracy,
+                "confusion_matrix": self.confusion_mat,
                 "summary": tf.summary.merge([tf.summary.scalar("valid_loss", self.loss),
                                              tf.summary.scalar("valid_accuracy", self.q_8_accuracy)],
                                             name="valid_summary"),
@@ -492,7 +495,7 @@ class Model(object):
 
             true_labels = tf.add(tf.cast(tf.argmax(one_hot_labels, axis=1), dtype=tf.int32),
                                  tf.reduce_sum(one_hot_labels, axis=1))
-            conf_mat = tf.confusion_matrix(logits_preds, true_labels, num_classes=9)
+            conf_mat = tf.confusion_matrix(true_labels, logits_preds, num_classes=9)
             conf_mat_8 = tf.slice(conf_mat, begin=[1, 1], size=[8, 8], name="confusion_mat8")
 
             tps = tf.diag_part(conf_mat_8, name="true_positives")
@@ -504,6 +507,7 @@ class Model(object):
             example_count = tf.slice(tf.shape(logits_ss), [0], [1])
             tf.summary.scalar("accuracy", q_8)
         self.q_8_accuracy = q_8
+        self.confusion_mat = conf_mat
         return q_8, example_count
 
     ##################################

training.py
@@ -23,27 +23,11 @@ tf.app.flags.DEFINE_integer('epoch_num', 10000,
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
 
+tf.app.flags.DEFINE_boolean('is_seq2seq_model', False,
+                            """Whether to log device placement.""")
+
 tf.app.flags.DEFINE_integer("batch_size", 64,
                             "number of batches")
-
-
-
-def evaluate(session, e_model):
-    """
-
-    :param session:
-    :param e_model:
-    :return:
-    """
-    e_val = 0
-    example_count = 0
-    for i in range(e_model.batch_per_epoch):
-        ret = session.run(e_model.fetches)
-        e_val += ret["evaluation"]
-        example_count += ret["example_count"]
-    else:
-        e_val /= i
-    print("evaluate %d proteins, get accuracy: %f" % (example_count/net_config.seq_len, e_val))
 
 
 class EarlyStop(object):
@@ -82,14 +66,18 @@ def main():
     early_stop = EarlyStop(30)
     gf = tf.Graph()
     with gf.as_default():
-        #train_model = Model(FLAGS, "train", ["/home/dm/data_sets/cullpdb+profile_6133_filtered_train.tfrecords"])
-        train_model = Seq2seqModel(FLAGS, "train", ["/home/dm/data_sets/cullpdb+profile_6133_filtered_train.tfrecords"])
+        if not FLAGS.is_seq2seq_model:
+            train_model = Model(FLAGS, "train", ["/home/dm/data_sets/cullpdb+profile_6133_filtered_train.tfrecords"])
+        else:
+            train_model = Seq2seqModel(FLAGS, "train", ["/home/dm/data_sets/cullpdb+profile_6133_filtered_train.tfrecords"])
         train_model.build_graph()
         ft = train_model.fetches
 
         with tf.name_scope("valid"):
-            #valid_model = Model(FLAGS, "valid")
-            valid_model = Seq2seqModel(FLAGS, "valid")
+            if not FLAGS.is_seq2seq_model:
+                valid_model = Model(FLAGS, "valid")
+            else:
+                valid_model = Seq2seqModel(FLAGS, "valid")
             valid_model.build_graph()
 
         valid_saver = tf.train.Saver(max_to_keep=1, name="valid_saver")
@@ -123,7 +111,9 @@ def main():
                         break
 
                     sv.summary_computed(sess, ret["summary"])
-                    print(ret["loss"], ret["evaluation"])
+
+                    print(ret["loss"], ret["evaluation"], np.sum(ret["confusion_matrix"]))
+                    print(ret["confusion_matrix"])
 
 
 