fix bug: correct the calculation of zero padding in accuracy

BasicModel.py
@@ -188,7 +188,8 @@ class Model(object):
                 "summary": tf.summary.merge([tf.summary.scalar("valid_loss", self.loss),
                                              tf.summary.scalar("valid_accuracy", self.q_8_accuracy)],
                                             name="valid_summary"),
-                "step": self.global_step
+                "step": self.global_step,
+                "debug": [self.debug_outptu, self.logits_ss]
             }
         elif self.mode == "inference":
             self.build_inference(self.seq_features, self.profile, is_reuse=False)
@@ -488,21 +489,26 @@ class Model(object):
         :return:
         """
         with tf.variable_scope("accuracy"):
-            logits_ss = tf.nn.softmax(logits_ss)
-            logits_preds = tf.add(tf.cast(tf.argmax(logits_ss, axis=1), dtype=tf.int32),
-                                  tf.cast(
-                                      tf.round(tf.reduce_sum(logits_ss, axis=1)),
-                                      dtype=tf.int32)
+            _argmax_logits_ss = tf.argmax(tf.nn.softmax(logits_ss), axis=1)
+            _sum_logits_ss = tf.reduce_sum(logits_ss, axis=1)
+            _shift = tf.cast(
+                tf.greater(_sum_logits_ss, tf.zeros_like(_sum_logits_ss)),
+                tf.int32
+            )
+            self.debug_outptu = logits_ss
+            logits_preds = tf.add(tf.cast(_argmax_logits_ss, dtype=tf.int32),
+                                  _shift
                                   )
 
             true_labels = tf.add(tf.cast(tf.argmax(one_hot_labels, axis=1), dtype=tf.int32),
                                  tf.reduce_sum(one_hot_labels, axis=1))
             conf_mat = tf.confusion_matrix(true_labels, logits_preds, num_classes=9)
             conf_mat_8 = tf.slice(conf_mat, begin=[1, 1], size=[8, 8], name="confusion_mat8")
+            predict_zero_case = tf.slice(conf_mat, begin=[1, 0], size=[8, 1])
 
             tps = tf.diag_part(conf_mat_8, name="true_positives")
             true_positive = tf.cast(tf.reduce_sum(tps), tf.float32)
-            total = tf.cast(tf.reduce_sum(conf_mat_8), tf.float32)
+            total = tf.cast(tf.reduce_sum(conf_mat_8) + tf.reduce_sum(predict_zero_case), tf.float32)
             q_8 = tf.div(true_positive, total, "accuracy_q8")
 
             # additional info for debug and monitor

seq2seq_model.py
@@ -67,6 +67,8 @@ class Seq2seqModel(Model):
         self.encoder_output = None
         self.encoder_final_state = None
         self.decoder_output = None
+        # max sequence length in current batch
+        self.curr_max_len = None
 
     def split_labels(self, labels):
         """
@@ -90,6 +92,7 @@ class Seq2seqModel(Model):
         super(Seq2seqModel, self).build_input()
         # adjust the shape
         _max_len = tf.reduce_max(self.seq_lens)
+        self.curr_max_len = _max_len
         self.labels_ss = tf.slice(self.labels_ss, [0, 0, 0], [-1, _max_len, -1])
         self.labels_sa = tf.slice(self.labels_sa, [0, 0, 0], [-1, _max_len, -1])
         # need to reshape the labels in order to set the last dimension to a fix size.
@@ -210,12 +213,12 @@ class Seq2seqModel(Model):
                 start_code = np.ones(self.net_config.output_size)
                 end_code = np.zeros(self.net_config.output_size)
                 embed_mat = np.identity(self.net_config.output_size)
-                embed_mat = np.vstack([start_code, end_code, embed_mat])
+                #embed_mat = np.vstack([start_code, end_code, embed_mat])
                 embed_mat = tf.convert_to_tensor(embed_mat, dtype=tf.float32)
                 # start_tokens vector of ones, length is batch size
                 start_tokens = tf.ones(tf.slice(tf.shape(seq_len), [0], [1]), dtype=tf.int32)
                 # end token is a scalar
-                end_token = 0
+                end_token = 10 # set a impossible number that force decoding to the end
                 helper = helper_py.GreedyEmbeddingHelper(embed_mat, start_tokens, end_token)
 
             # get rnn cell for decoder
@@ -227,7 +230,9 @@ class Seq2seqModel(Model):
                 #initial_state=cell.zero_state(
                 #    dtype=dtypes.float32, batch_size=self.batch_size),
                 initial_state=encoder_final_state,
-                output_layer=layers_core.Dense(self.net_config.output_size, use_bias=False))
+                output_layer=layers_core.Dense(self.net_config.output_size, activation=tf.nn.relu,
+                                               use_bias=False)
+            )
 
             batch_max_len = tf.reduce_max(seq_len)
             # build the decoder layer
@@ -261,6 +266,11 @@ class Seq2seqModel(Model):
                 logits_sa = None
             self.logits_ss = logits_ss
             self.logits_sa = logits_sa
+
+            if not self.is_training:
+                # padding the output to the input length
+                _curr_len = tf.shape(self.logits_ss)[1]
+                self.logits_ss = tf.pad(self.logits_ss, [[0, 0], [0, self.curr_max_len - _curr_len], [0, 0]])
 
         return logits_ss, logits_sa
 

utility_test.py
@@ -205,6 +205,7 @@ def test_something():
     with graph.as_default():
         input_pl = tf.placeholder(tf.float32, (None, None, 3))
         _input_pl2 = tf.placeholder(tf.float32, (None, ))
+        _scaler_pl = tf.placeholder(tf.int32, [1])
         _input = tf.convert_to_tensor(input_pl)
         _input_slice = tf.slice(_input, [0, 0, 0], [-1, 1, -1])
         _slice_shape = tf.shape(_input_slice)
@@ -217,6 +218,17 @@ def test_something():
         batch_size2 = tf.ones([tf.shape(_input_pl2)[0]])
         slice_input = tf.slice(_input_c, [0, 0, 0], [-1, batch_size[0], -1])
         slice_input = tf.reshape(slice_input, [batch_size[0], -1, 3])
+        pad_slice = tf.convert_to_tensor([[[0, 0, 0]]], dtype=tf.float32)
+        pad = tf.tile(pad_slice, [batch_size[0], 4 - tf.shape(slice_input)[0], 1])
+        pad_output = tf.concat([slice_input, pad], axis=1)
+        output_tokens = tf.reduce_sum(pad_output, axis=2)
+        zero_tensor = tf.zeros_like(output_tokens)
+        cond = tf.cast(tf.greater(output_tokens, zero_tensor), tf.int32)
+
+        embed_mat = np.identity(4)
+        embed_mat = tf.convert_to_tensor(embed_mat, dtype=tf.int32)
+        embed = tf.nn.embedding_lookup(embed_mat, _scaler_pl)
+
 
     # Create a session for running operations in the Graph.
     with tf.Session(graph=graph) as session:
@@ -224,12 +236,15 @@ def test_something():
         # Initialize the variables (the trained variables and the
         # epoch counter).
         session.run(init_op)
-        for i in range(1, 4):
+        for i in range(1, 5):
             value = np.asarray([[[i]*3]*4]*i, dtype=np.float32)
             value2 = np.asarray([i]*i, dtype=np.float32)
-            fd = {input_pl: value, _input_pl2: value2}
-            ret = session.run(slice_input, feed_dict=fd)
-            print(ret)
+            fd = {input_pl: value, _input_pl2: value2, _scaler_pl: [i]}
+            ret = session.run([embed], feed_dict=fd)
+            print("###################")
+            print(ret[0])
+            print("#")
+            #print(ret[1])
 
 
 